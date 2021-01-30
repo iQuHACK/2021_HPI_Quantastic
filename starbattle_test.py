fe2e7c5d29df17758c2ae2051f09d24573a90b0c
Alexander Sohn <alexander.sohn642@gmail.com>
2021-01-30 18:19:56 +0100

    Change example_cells.txt and add test for get_cells

--- starbattle_test.py
+++ starbattle_test.py
@@ -1,2 +1,12 @@
+from starbattle import get_cells
+
+
+def test_get_cells():
+    filename = "example_cells.txt"
+    cells = get_cells(filename)
+    assert cells == [[0, 0, 1, 1, 1], [0, 0, 1, 2, 2], [
+        3, 3, 1, 2, 2], [3, 3, 3, 2, 4], [3, 3, 4, 4, 4]]
+
+
 def test_test():
     assert 3 == 3
