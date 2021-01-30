782d3ee0a8e2f7904599f424c6904b92bc7dfbbe
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2021-01-30 20:44:37 +0100

    Add better output

--- starbattle.py
+++ starbattle.py
@@ -1,9 +1,9 @@
+import sys
+from collections import defaultdict
 import dimod
 from dimod.generators.constraints import combinations
-from collections import defaultdict
 from hybrid.reference import KerberosSampler
-import sys
-
+from colorama import init, Fore
 
 def get_cells(filename):
     """Return a list of lists containing the content of the input text file.
@@ -26,9 +26,9 @@ def get_cells(filename):
 
     return lines
 
-def neighbors(y, x, height, width):
+def neighbors(y, x, width):
     start_y = 0 if y == 0 else y-1
-    end_y = height-1 if y == height-1 else y+1
+    end_y = width-1 if y == width-1 else y+1
 
     start_x = 0 if x == 0 else x-1
     end_x = width-1 if x == width-1 else x+1
@@ -41,14 +41,13 @@ def neighbors(y, x, height, width):
     return neighbors
 
 def has_neighboring_star(solution, y, x):
-    for neighbor in neighbors(y, x, len(solution), len(solution[0])):
+    for neighbor in neighbors(y, x, len(solution)):
         if solution[neighbor[0]][neighbor[1]] == 1 and (neighbor[0] != y or neighbor[1] != x):
             return True
     return False
 
 def verify_solution(n, cells, solution):
-    height = len(cells)
-    width = len(cells[0])
+    width = len(cells)
 
     # Step 1: verify that n stars / row
     for row in solution:
@@ -78,28 +77,25 @@ def verify_solution(n, cells, solution):
             return False
 
     # Step 4: verify that no stars are adjacent
-    for y in range(height):
+    for y in range(width):
         for x in range(width):
             if solution[y][x] and has_neighboring_star(solution, y, x):
                 return False
 
     return True
 
-def get_label(y, x):
-    return '{},{}'.format(y,x)
-
 def build_bqm(cells, n):
     bqm = dimod.BinaryQuadraticModel({}, {}, 0.0, dimod.BINARY)
 
     # constraint 1: n stars per row
     for y, row in enumerate(cells):
-        row_labels = [get_label(y,x) for x,_ in enumerate(row)]
+        row_labels = [(y,x) for x,_ in enumerate(row)]
         row_bqm = combinations(row_labels, n)
         bqm.update(row_bqm)
 
     # constraint 2: n stars per column
     for x in range(len(cells[0])):
-        col_labels = [get_label(y, x) for y in range(len(cells))]
+        col_labels = [(y,x) for y in range(len(cells))]
         col_bqm = combinations(col_labels, n)
         bqm.update(col_bqm)
 
@@ -107,7 +103,7 @@ def build_bqm(cells, n):
     block_to_labels = defaultdict(list)
     for y, row in enumerate(cells):
         for x, cell in enumerate(row):
-            block_to_labels[cell].append(get_label(y, x))
+            block_to_labels[cell].append((y,x))
 
     for block in block_to_labels:
         block_bqm = combinations(block_to_labels[block], n)
@@ -116,12 +112,49 @@ def build_bqm(cells, n):
     # constraint 4: no adjacent stars
     for y, row in enumerate(cells):
         for x, _ in enumerate(row):
-            for neighbor in neighbors(y, x, len(cells), len(cells[0])):
-                bqm.add_interaction(get_label(y,x), get_label(neighbor[0], neighbor[1]), 1)
+            for neighbor in neighbors(y, x, len(cells)):
+                bqm.add_interaction((y,x), (neighbor[0],neighbor[1]), 1)
     return bqm
 
+def sample_to_solution(sample, width):
+    solution = [[ 0 for _ in range(width) ] for _ in range(width)]
+    for coords in sample:
+        solution[coords[0]][coords[1]] = sample[coords]
+    return solution
+
+def print_solution(solution):
+    for row in solution:
+        for cell in row:
+            if cell == 1:
+                print(Fore.YELLOW + "*" + Fore.RESET, end=" ")
+            else:
+                print("- ", end="")
+        print(Fore.RESET)
+
+def print_cells(cells):
+    SYMBOLS = {
+        0: ('/', Fore.RED),
+        1: ('$', Fore.GREEN),
+        2: ('?', Fore.YELLOW),
+        3: ('|', Fore.BLUE),
+        4: ('%', Fore.MAGENTA),
+        5: ('(', Fore.CYAN),
+        6: ('§', Fore.WHITE),
+        7: ('+', Fore.LIGHTRED_EX),
+        8: ('_', Fore.LIGHTGREEN_EX),
+        9: ('&', Fore.LIGHTYELLOW_EX),
+        10: ('~', Fore.LIGHTBLUE_EX),
+        11: ('#', Fore.LIGHTMAGENTA_EX),
+        12: ('^', Fore.LIGHTCYAN_EX),
+        13: ('=', Fore.LIGHTWHITE_EX),
+    }
+    for y,row in enumerate(cells):
+        for x,cell in enumerate(row):
+            print(SYMBOLS[cell][1] + SYMBOLS[cell][0] if cell < len(SYMBOLS) else cell, end=" ")
+        print(Fore.RESET)
 
 if __name__ == "__main__":
+
     # Read user input
     if len(sys.argv) > 1:
         filename = sys.argv[1]
@@ -130,8 +163,20 @@ if __name__ == "__main__":
         print("Warning: using default problem file, '{}'. Usage: python "
               "{} <cells filepath>".format(filename, sys.argv[0]))
 
-    cells = get_cells(filename)
-    bqm = build_bqm(cells, 1)
 
-    solution = KerberosSampler().sample(bqm, max_iter=10, qpu_params={'label': 'Starbattle'})
-    print(solution.first.sample)
+    cells = get_cells(filename)
+
+    init()
+    print("problem:")
+    print_cells(cells)
+    print()
+
+    bqm = build_bqm(cells, 1)
+    sampleset = KerberosSampler().sample(bqm, max_iter=10, qpu_params={'label': 'Starbattle'})
+    solution = sample_to_solution(sampleset.first.sample, len(cells))
+
+    if verify_solution(1, cells, solution):
+        print("found valid solution:")
+    else:
+        print("found invalid solution:")
+    print_solution(solution)
