5c9c015a6e7da37ebe464393091afc99c83c2659
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2021-01-30 23:57:29 +0100

    Fix tests

--- starbattle_test.py
+++ starbattle_test.py
@@ -15,10 +15,10 @@ def test_get_cells():
 def test_verify_simple():
     matrix = [[0]]
     solution = [[1]]
-    assert verify_solution(1, matrix, solution)
+    assert verify_solution(1, matrix, solution)[0]
 
     solution2 = [[0]]
-    assert not verify_solution(1, matrix, solution2)
+    assert not verify_solution(1, matrix, solution2)[0]
 
 def test_verify_5():
     matrix = [[0,0,1,1,1],
@@ -32,32 +32,32 @@ def test_verify_5():
                 [0,0,0,1,0],
                 [1,0,0,0,0],
                 [0,0,1,0,0]]
-    assert verify_solution(1, matrix, solution)
+    assert verify_solution(1, matrix, solution)[0]
 
     solution2 = [[0,0,0,0,1],
                 [0,1,0,0,0],
                 [0,0,0,1,0],
                 [1,0,0,0,0],
                 [0,0,0,1,0]]
-    assert not verify_solution(1, matrix, solution2)
+    assert not verify_solution(1, matrix, solution2)[0]
 
     solution3 = [[0,1,0,0,1],
                 [0,0,0,0,0],
                 [0,0,0,1,0],
                 [1,0,0,0,0],
                 [0,0,1,0,0]]
-    assert not verify_solution(1, matrix, solution3)
+    assert not verify_solution(1, matrix, solution3)[0]
 
     solution4 = [[0,0,0,0,1],
                 [0,0,1,0,0],
                 [1,0,0,0,0],
                 [0,0,0,1,0],
                 [0,1,0,0,0]]
-    assert not verify_solution(1, matrix, solution4)
+    assert not verify_solution(1, matrix, solution4)[0]
 
     solution5 = [[0,0,0,0,1],
                 [0,1,0,0,0],
                 [1,0,0,0,0],
                 [0,0,0,1,0],
                 [0,0,1,0,0]]
-    assert not verify_solution(1, matrix, solution5)
+    assert not verify_solution(1, matrix, solution5)[0]
