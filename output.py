cb0eaf71c947df340fa1a7dc75f6f925a7247074
Richard Wohlbold <richard.wohlbold@student.hpi.de>
2021-01-31 10:33:45 +0100

    Make output prettier

--- output.py
+++ output.py
@@ -1,35 +1,36 @@
 from colorama import init as colorama_init, Fore
 from starbattle import full_solution, verify_solution
 
+SYMBOLS = {
+    0: ('/', Fore.RED),
+    1: ('$', Fore.GREEN),
+    2: ('?', Fore.YELLOW),
+    3: ('|', Fore.BLUE),
+    4: ('%', Fore.MAGENTA),
+    5: ('(', Fore.CYAN),
+    6: ('§', Fore.WHITE),
+    7: ('+', Fore.LIGHTRED_EX),
+    8: ('_', Fore.LIGHTGREEN_EX),
+    9: ('&', Fore.LIGHTYELLOW_EX),
+    10: ('~', Fore.LIGHTBLUE_EX),
+    11: ('#', Fore.LIGHTMAGENTA_EX),
+    12: ('^', Fore.LIGHTCYAN_EX),
+    13: ('=', Fore.LIGHTWHITE_EX),
+}
+
 def init():
     colorama_init()
 
-def print_solution(solution):
-    for row in solution:
-        for cell in row:
+def print_solution(cells, solution):
+    for y,row in enumerate(solution):
+        for x,cell in enumerate(row):
             if cell == 1:
                 print(Fore.YELLOW + "*" + Fore.RESET, end=" ")
             else:
-                print("- ", end="")
+                print(SYMBOLS[cells[y][x]][1] + "- " if cells[y][x] < len(SYMBOLS) else Fore.RESET + "- ", end="")
         print(Fore.RESET)
 
 def print_cells(cells):
-    SYMBOLS = {
-        0: ('/', Fore.RED),
-        1: ('$', Fore.GREEN),
-        2: ('?', Fore.YELLOW),
-        3: ('|', Fore.BLUE),
-        4: ('%', Fore.MAGENTA),
-        5: ('(', Fore.CYAN),
-        6: ('§', Fore.WHITE),
-        7: ('+', Fore.LIGHTRED_EX),
-        8: ('_', Fore.LIGHTGREEN_EX),
-        9: ('&', Fore.LIGHTYELLOW_EX),
-        10: ('~', Fore.LIGHTBLUE_EX),
-        11: ('#', Fore.LIGHTMAGENTA_EX),
-        12: ('^', Fore.LIGHTCYAN_EX),
-        13: ('=', Fore.LIGHTWHITE_EX),
-    }
     for y,row in enumerate(cells):
         for x,cell in enumerate(row):
             print(SYMBOLS[cell][1] + SYMBOLS[cell][0] if cell < len(SYMBOLS) else cell, end=" ")
@@ -47,4 +48,4 @@ def solve_with_output(cells, num_stars):
         print("found valid solution:")
     else:
         print("found invalid solution because {}:".format(err))
-    print_solution(solution)
+    print_solution(cells, solution)
