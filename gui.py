2790eba3abff0796ddc0d5380d824f4d88033ac1
Alexander Sohn <alexander.sohn642@gmail.com>
2021-01-31 10:09:25 +0100

    Refactor gui

--- gui.py
+++ gui.py
@@ -4,8 +4,45 @@ from starbattle import full_solution, verify_solution
 from queue import Queue
 from threading import Thread
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+
+size = (1080, 1280)
+
+
 def full_solve_queue(q, cells, num_stars):
     q.put(full_solution(cells, num_stars))
+
+
+def draw_board(screen, cells, marked_cells, show_solution, solution, delta_x, delta_y):
+    screen.fill(WHITE)
+    sysfont = pygame.font.get_default_font()
+    plus_font = pygame.font.SysFont(sysfont, round(size[0]/7))
+    mark = plus_font.render("+", True, BLACK)
+    start_y = 0
+    for y, row in enumerate(cells):
+        start_x = 0
+        for x, cell in enumerate(row):
+            if show_solution and solution[y][x] == 1:
+                pygame.draw.rect(screen, GREEN, [
+                                 start_x, start_y, delta_x, delta_y])
+
+            if marked_cells[y][x] == 1:
+                text_rect = mark.get_rect(
+                    center=(start_x + (delta_x/2), start_y + (delta_y/2)))
+                screen.blit(mark, text_rect)
+
+            if y >= 1:
+                pygame.draw.line(screen, BLACK, [start_x, start_y],
+                                 [start_x + delta_x, start_y], 2 if cells[y-1][x] == cells[y][x] else 5)
+            if x >= 1:
+                pygame.draw.line(screen, BLACK, [start_x, start_y],
+                                 [start_x, start_y+delta_y], 2 if cells[y][x-1] == cells[y][x] else 5)
+            start_x += delta_x
+        start_y += delta_y
+
 
 def loop(cells, num_stars):
 
@@ -13,7 +50,6 @@ def loop(cells, num_stars):
     thread = Thread(target=full_solve_queue, args=(q, cells, num_stars))
     thread.start()
     pygame.init()
-    size = (1080, 1280)
     screen = pygame.display.set_mode(size)
     width = len(cells)
 
@@ -21,20 +57,16 @@ def loop(cells, num_stars):
     solution = [[0]*width for _ in range(width)]
 
     # Define some colors
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    YELLOW = (255, 255, 0)
-    GREEN = (0, 255, 0)
 
     button_height = 200
 
     delta_x = size[0] / width
     delta_y = (size[1] - button_height) / width
     # The loop will carry on until the user exit the game (e.g. clicks the close button).
-    carryOn = True
+    carry_on = True
     sysfont = pygame.font.get_default_font()
     font = pygame.font.SysFont(sysfont, round(size[0]/22.5))
-    showSolution = False
+    show_solution = False
     # The clock will be used to control how fast the screen updates
     clock = pygame.time.Clock()
     # -------- Main Program Loop -----------
@@ -43,14 +75,15 @@ def loop(cells, num_stars):
     else:
         status = "You need {} stars per block, row and column".format(
             num_stars)
-    text = font.render(status, True, BLACK)
+    win_text = font.render(status, True, BLACK)
+    solution = []
     valid = False
-    while carryOn:
+    while carry_on:
         # --- Main event loop
         mouse = pygame.mouse.get_pos()
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
-                carryOn = False  # Flag that we are done so we exit this loop
+                carry_on = False  # Flag that we are done so we exit this loop
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if mouse[1] < size[1] - button_height:
                     cell_y = int(mouse[1] / delta_y)
@@ -59,41 +92,19 @@ def loop(cells, num_stars):
                     marked_cells[cell_y][cell_x] = 0 if marked_cells[cell_y][cell_x] == 1 else 1
                 valid, error = verify_solution(num_stars, cells, marked_cells)
                 if valid:
-                    carryOn = False
+                    carry_on = False
                 else:
-                    text = font.render(error, True, BLACK)
+                    win_text = font.render(error, True, BLACK)
         if q.qsize() > 0:
             solution = q.get()
-            showSolution = True
+            show_solution = True
             print("solution available, it's", solution)
-        screen.fill(WHITE)
-        start_y = 0
-        for y, row in enumerate(cells):
-            start_x = 0
-            for x, cell in enumerate(row):
-                if showSolution and solution[y][x] == 1:
-                    pygame.draw.rect(screen, GREEN, [
-                                     start_x, start_y, delta_x, delta_y])
 
-                if marked_cells[y][x] == 1:
-                    font1 = pygame.font.SysFont(sysfont, round(size[0]/7))
-                    text1 = font1.render("+", True, BLACK)
-                    text_rect = text1.get_rect(center=(start_x + (delta_x/2), start_y + (delta_y/2)))
-                    screen.blit(text1, text_rect)
-                    
-                if y >= 1:
-                    pygame.draw.line(screen, BLACK, [start_x, start_y],
-                                     [start_x + delta_x, start_y], 2 if cells[y-1][x] == cells[y][x] else 5)
-                if x >= 1:
-                    pygame.draw.line(screen, BLACK, [start_x, start_y],
-                                     [start_x, start_y+delta_y], 2 if cells[y][x-1] == cells[y][x] else 5)
-                start_x += delta_x
-
-            start_y += delta_y
-
-        text_rect = text.get_rect(
+        draw_board(screen, cells, marked_cells,
+                   show_solution, solution, delta_x, delta_y)
+        text_rect = win_text.get_rect(
             center=(size[0]/2, size[1]-(button_height/2)))
-        screen.blit(text, text_rect)
+        screen.blit(win_text, text_rect)
 
         pygame.draw.rect(
             screen, BLACK, [0, size[1]-button_height, size[0], size[1]], width=3)
@@ -103,9 +114,9 @@ def loop(cells, num_stars):
         clock.tick(60)
     if valid:
         font = pygame.font.SysFont(sysfont, round(size[0]/11))
-        text = font.render("You WON!", True, BLACK)
-        text_rect = text.get_rect(center=(size[0] / 2, size[1] / 2))
-        screen.blit(text, text_rect)
+        win_text = font.render("You WON!", True, BLACK)
+        text_rect = win_text.get_rect(center=(size[0] / 2, size[1] / 2))
+        screen.blit(win_text, text_rect)
         pygame.display.flip()
         time.sleep(4)
     # Once we have exited the main program loop we can stop the game engine:
